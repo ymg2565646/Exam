00e13efd6cdcbc6c7c447e0e943cbed989100609
student <student@LAPTOP-8DRRE8LS>
2026-05-14 20:50:15 +0900

    修正514

--- src/main/java/bean/TestListSubject.java
+++ src/main/java/bean/TestListSubject.java
@@ -13,12 +13,12 @@ public class TestListSubject implements Serializable {
 }
 	public int getEntYear() { return entYear; }
 	public void setEntYear(int entYear) { this.entYear = entYear; }
-	public String getstudentNo() { return studentNo; }
-	public void getstudentNo(String studentNo) { this.studentNo = studentNo; }
-	public String getstudentName() { return studentName; }
-	public void getstudentName(String studentName) { this.studentName = studentName; }
-	public String getclassNum() { return classNum; }
-	public void getclassNum(String classNum) { this.classNum = classNum; }
-	public int  getpoint() { return point; }
-	public void getpoint(int point) { this.point = point; }
+	public String getStudentNo() { return studentNo; }
+	public void setStudentNo(String studentNo) { this.studentNo = studentNo; }
+	public String getStudentName() { return studentName; }
+	public void setStudentName(String studentName) { this.studentName = studentName; }
+	public String getClassNum() { return classNum; }
+	public void setClassNum(String classNum) { this.classNum = classNum; }
+	public int  getPoint() { return point; }
+	public void setPoint(int point) { this.point = point; }
 }
